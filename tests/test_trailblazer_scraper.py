ed2cd8792073a73be797337fd29d45d14c2f8a10
Aaron George <angeorge.dev@gmail.com>
2020-12-13 22:02:27 -0500

    Add tests to evaluate the Profile class

--- tests/test_trailblazer_scraper.py
+++ tests/test_trailblazer_scraper.py
@@ -1,5 +1,22 @@
-from trailblazer_scraper import __version__
+from trailblazer_scraper import Profile
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+def test_tbid_provided():
+    test_tbid = "testtbid"
+    u = Profile("ecastelli", test_tbid)
+    assert u.tbid == test_tbid
+
+
+def test_scrape_basics():
+    u = Profile("ecastelli")
+    assert u.first_name == "Emily"
+    assert u.last_name == "Castelli"
+    assert len(u.tbid) == 18
+
+
+def test_fetch_rank_details():
+    u = Profile("ecastelli")
+    assert u.rank_data is None
+    u.fetch_rank_details()
+    assert u.rank_data is not None
+    assert "Id" in u.rank_data
