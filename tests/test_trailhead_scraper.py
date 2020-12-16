962e19d4ee2bfc8b3a45a75bac4ad76cf68c2a56
Aaron George <75114114+ang3orge@users.noreply.github.com>
2020-12-16 01:22:58 -0500

    Refactor and Error Handling (#2)
    * Implement error handling for _get_tbid and remove first_name, last_name from the Profile class
    
    * Add a method to retrieve profile data in JSON format
    
    * Make base URLs global variables
    
    * Rename AuraPayload to _AuraPayload
    
    * Rename Profile to TrailheadProfile
    
    * Add function to check for errors in aura action responses
    
    * Extract private helper methods out of the TrailheadProfile class
    
    * Minor change to _get_aura_response_body docstring

--- tests/test_trailhead_scraper.py
+++ tests/test_trailhead_scraper.py
@@ -1,21 +1,48 @@
-from trailhead_scraper import Profile
+import pytest
+from trailhead_scraper import TrailheadProfile
 
 
 def test_tbid_provided():
     test_tbid = "testtbid"
-    u = Profile("ecastelli", test_tbid)
+    u = TrailheadProfile("ecastelli", test_tbid)
     assert u.tbid == test_tbid
 
 
-def test_scrape_basics():
-    u = Profile("ecastelli")
-    assert u.first_name == "Emily"
-    assert u.last_name == "Castelli"
+def test_correct_username():
+    u = TrailheadProfile("ecastelli")
     assert len(u.tbid) == 18
 
 
+def test_incorrect_username():
+    with pytest.raises(Exception):
+        TrailheadProfile("incorrect_username")
+
+
+def test_incorrect_tbid_fetch_rank_data():
+    with pytest.raises(Exception):
+        u = TrailheadProfile("ecastelli", "1234567890")
+        u.fetch_rank_data()
+
+
+def test_incorrect_tbid_fetch_awards():
+    with pytest.raises(Exception):
+        u = TrailheadProfile("ecastelli", "1234567890")
+        u.fetch_awards()
+
+
+def test_fetch_profile_data():
+    u = TrailheadProfile("ecastelli")
+    assert u.profile_data is None
+    u.fetch_profile_data()
+    assert u.profile_data is not None
+    assert "profileUser" in u.profile_data
+
+    assert u.profile_data["profileUser"]["FirstName"] == "Emily"
+    assert u.profile_data["profileUser"]["LastName"] == "Castelli"
+
+
 def test_fetch_rank_data():
-    u = Profile("ecastelli")
+    u = TrailheadProfile("ecastelli")
     assert u.rank_data is None
     u.fetch_rank_data()
     assert u.rank_data is not None
@@ -23,7 +50,7 @@ def test_fetch_rank_data():
 
 
 def test_fetch_awards():
-    u = Profile("ecastelli")
+    u = TrailheadProfile("ecastelli")
     assert len(u.awards) <= 0
     u.fetch_awards()
     assert len(u.awards) > 0
--- trailhead_scraper.py
+++ trailhead_scraper.py
@@ -4,8 +4,11 @@ import json
 
 __version__ = "0.1.0"
 
+base_profile_url = "https://trailblazer.me"
+aura_service_url = "https://trailblazer.me/aura"
 
-class AuraPayload:
+
+class _AuraPayload:
     """Represents a request payload for Aura Services"""
 
     def __init__(self, uri, action_descriptor=None):
@@ -45,7 +48,8 @@ class AuraPayload:
             }
         )
 
-    def json(self):
+    @property
+    def data(self):
         """Return the payload in an appropriate format.
 
         Returns:
@@ -59,11 +63,45 @@ class AuraPayload:
         }
 
 
-class Profile:
-    """A collection of user details, rank data, and awards collected from a Trailhead profile."""
+def _get_tbid(url):
+    """Retrieve the Trailblazer ID for the user by scraping the profile page."""
+    page = requests.get(url)
 
-    base_url = "https://trailblazer.me"
-    aura_url = base_url + "/aura"
+    try:
+        return re.search(r"User\/(.*?)\\", page.text).group(1)
+    except:
+        # raise an exception if the tbid is not found - this prevents any of the other methods from being executed
+        raise Exception(
+            "The TBID was not found for '{}'. The provided URL may be incorrect or the profile may be private.".format(
+                url
+            )
+        )
+
+
+def _get_aura_response_body(payload):
+    """Perform the Aura Service POST request and return the parsed response body.
+
+    Args:
+        payload (_AuraPayload): The data that will be sent with the POST request.
+
+    Returns:
+        dict: The parsed response body.
+    """
+    response = requests.post(aura_service_url, data=payload)
+
+    j = response.json()
+
+    for action in j["actions"]:
+        if action["state"] == "ERROR":
+            raise Exception(
+                "Aura Action Error: {}".format(action["error"][0]["message"])
+            )
+
+    return json.loads(j["actions"][0]["returnValue"]["returnValue"]["body"])
+
+
+class TrailheadProfile:
+    """A collection of user details, rank data, and awards collected from a Trailhead profile."""
 
     def __init__(self, username, tbid=None):
         """Initialize the Trailhead profile.
@@ -74,43 +112,37 @@ class Profile:
         """
         self.username = username
         self.path = "/id/" + username
-        self.url = self.base_url + self.path
+        self.url = base_profile_url + self.path
         self.tbid = tbid
-        self.first_name = None
-        self.last_name = None
 
+        self.profile_data = None
         self.rank_data = None
         self.awards = []
 
         # if the Trailblazer ID is not provided, retrieve it from the profile page
         if tbid is None:
-            self._scrape_basics()
+            self.tbid = _get_tbid(self.url)
 
-    def _scrape_basics(self):
-        """Retrieve the Trailblazer ID, First Name, and Last Name for the current user by scraping the profile page."""
-        page = requests.get(self.url)
-
-        self.tbid = re.search(r"User\/(.*?)\\", page.text).group(1)
-        self.first_name = re.search(r'FirstName\\":\\"(.*?)\\', page.text).group(1)
-        self.last_name = re.search(r'LastName\\":\\"(.*?)\\', page.text).group(1)
-
-    def _get_aura_response_body(self, payload):
-        """Perform the Aura Service POST request and return the parsed response body.
+    def fetch_profile_data(self, keep_picklists=False):
+        """Retrieve all profile data for the Trailhead user.
 
         Args:
-            payload (AuraPayload): The payload that will be sent with the POST request.
-
-        Returns:
-            dict: The parsed response body.
+            keep_picklists (bool, optional): Keep the 'pickLists' attribute in the profile data (JSON) retrieved from the page. Defaults to False.
         """
-        response = requests.post(self.aura_url, data=payload)
+        page = requests.get(self.url)
 
-        j = json.loads(response.text)
-        return json.loads(j["actions"][0]["returnValue"]["returnValue"]["body"])
+        self.profile_data = json.loads(
+            re.search(r'profileData = JSON.parse\("(.*?)"\)', page.text)
+            .group(1)
+            .replace("\\", "")
+        )
+
+        if not keep_picklists and "pickLists" in self.profile_data:
+            del self.profile_data["pickLists"]
 
     def fetch_rank_data(self):
         """Retrieve rank information for the Trailhead user profile."""
-        payload = AuraPayload(self.path)
+        payload = _AuraPayload(self.path)
         payload.add_action(
             "TrailheadProfileService",
             "fetchTrailheadData",
@@ -119,7 +151,7 @@ class Profile:
             },
         )
 
-        body = self._get_aura_response_body(payload.json())
+        body = _get_aura_response_body(payload.data)
 
         self.rank_data = body["value"][0]["ProfileCounts"][0]
 
@@ -130,13 +162,13 @@ class Profile:
 
         for skip in range(0, self.rank_data["EarnedBadgeTotal"], 30):
 
-            payload = AuraPayload(self.path)
+            payload = _AuraPayload(self.path)
             payload.add_action(
                 "TrailheadProfileService",
                 "fetchTrailheadBadges",
                 {"userId": self.tbid, "skip": skip, "perPage": 30, "filter": "All"},
             )
 
-            body = self._get_aura_response_body(payload.json())
+            body = _get_aura_response_body(payload.data)
 
             self.awards = [*self.awards, *body["value"][0]["EarnedAwards"]]
